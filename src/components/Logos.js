6fbbc63fbbf7bbb17cad5d0bc3205d5955ecfc55
Ok-Cheese <starberry970310@gmail.com>
2022-02-09 20:21:40 +0900

    fix: Fix the problem that the city name is displayed before the mark is completely displayed

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -17,6 +17,13 @@ import logo12 from '../imgs/Marks/함흥.png';
 import logo13 from '../imgs/Marks/원산.png';
 
 function Logos() {
+  const [loaded, setLoaded] = useState(false);
+  // 마지막 로고가 나타난 후 loaded를 true로 변경.
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 3200);
+  }, []);
 
   return (
     <div id={styles.outer}>
@@ -30,10 +37,12 @@ function Logos() {
             className={`${styles.logo} ${styles.logo_center}`} src={logo1} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 800)}
           ></img>
-          <span
-            className={styles.cityName}
-            style={{ fontSize: "4rem"}}
-          >경성</span>
+          {
+            loaded ? 
+              <span className={styles.cityName} style={{ fontSize: "4rem"}}>
+                경성
+              </span> : ""
+          }
         </Link>
         <Link to={'/archive/0B'}
           className={styles.contianer_logo}
@@ -43,9 +52,9 @@ function Logos() {
             className={styles.logo} src={logo2} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 1000)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >인천</span>
+          {
+            loaded ? <span className={styles.cityName}>인천</span> : ""
+          }
         </Link>
         <Link to={'/archive/0C'} hidden
           className={styles.contianer_logo}
@@ -55,9 +64,9 @@ function Logos() {
             className={styles.logo} src={logo3} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 1200)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >대전</span>
+          {
+            loaded ? <span className={styles.cityName}>대전</span> : ""
+          }
         </Link>
         <Link to={'/archive/0D'} hidden
           className={styles.contianer_logo}
@@ -67,9 +76,9 @@ function Logos() {
             className={styles.logo} src={logo4} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 1400)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >대구</span>
+          {
+            loaded ? <span className={styles.cityName}>대구</span> : ""
+          }
         </Link>
         <Link to={'/archive/0E'} hidden
           className={styles.contianer_logo}
@@ -79,9 +88,9 @@ function Logos() {
             className={styles.logo} src={logo5} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 1600)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >부산</span>
+          {
+            loaded ? <span className={styles.cityName}>부산</span> : ""
+          }
         </Link>
         <Link to={'/archive/0F'} hidden
           className={styles.contianer_logo}
@@ -91,9 +100,9 @@ function Logos() {
             className={styles.logo} src={logo6} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 1800)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >군산</span>
+          {
+            loaded ? <span className={styles.cityName}>군산</span> : ""
+          }
         </Link>
         <Link to={'/archive/AA'} hidden
           className={styles.contianer_logo}
@@ -103,9 +112,9 @@ function Logos() {
             className={styles.logo} src={logo7} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 2000)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >목포</span>
+          {
+            loaded ? <span className={styles.cityName}>목포</span> : ""
+          }
         </Link>
         <Link to={'/archive/AB'} hidden
           className={styles.contianer_logo}
@@ -115,9 +124,9 @@ function Logos() {
             className={styles.logo} src={logo8} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 2200)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >광주</span>
+          {
+            loaded ? <span className={styles.cityName}>광주</span> : ""
+          }
         </Link>
         <Link to={'/archive/AC'} hidden
           className={styles.contianer_logo}
@@ -127,9 +136,9 @@ function Logos() {
             className={styles.logo} src={logo9} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 2400)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >평양</span>
+          {
+            loaded ? <span className={styles.cityName}>평양</span> : ""
+          }
         </Link>
         <Link to={'/archive/AD'} hidden
           className={styles.contianer_logo}
@@ -139,9 +148,9 @@ function Logos() {
             className={styles.logo} src={logo10} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 2600)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >청진</span>
+          {
+            loaded ? <span className={styles.cityName}>청진</span> : ""
+          }
         </Link>
         <Link to={'/archive/AE'} hidden
           className={styles.contianer_logo}
@@ -151,9 +160,9 @@ function Logos() {
             className={styles.logo} src={logo11} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 2800)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >신의주</span>
+          {
+            loaded ? <span className={styles.cityName}>신의주</span> : ""
+          }
         </Link>
         <Link to={'/archive/AF'} hidden
           className={styles.contianer_logo}
@@ -163,9 +172,9 @@ function Logos() {
             className={styles.logo} src={logo12} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 3000)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >함흥</span>
+          {
+            loaded ? <span className={styles.cityName}>함흥</span> : ""
+          }
         </Link>
         <Link to={'/archive/BA'} hidden
           className={styles.contianer_logo}
@@ -175,9 +184,9 @@ function Logos() {
             className={styles.logo} src={logo13} hidden
             onLoad={(e) => setTimeout(() => e.target.hidden = false, 3200)}
           ></img>
-          <span 
-            className={styles.cityName}
-          >원산</span>
+          {
+            loaded ? <span className={styles.cityName}>원산</span> : ""
+          }
         </Link>
       </section>
     </div>
