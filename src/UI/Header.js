114fbcadec67819ae1883c82830c4adeaaa1ec52
Ok-Cheese <starberry970310@gmail.com>
2022-03-27 18:56:46 +0900

    Refactor(Header_Archive)

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import NormalHeader from './NormalHeader';
 import MarkHeader from './MarkHeader';
 
 import btnMenu from '../imgs/menu.png';
-import btnMenu_rev from '../imgs/menu_rev.png';
 import logo_reverse from '../imgs/logo_bold_rev.png';
 import logo_aap from '../imgs/logo_bold.png';
 import classes from './Header.module.css';
@@ -28,27 +27,34 @@ function Header() {
   
 
   return (
-    <header className={classes.header}>
-      <Link to={'/'}>
-        <img className={classes.logo} src={isOpened ? logo_reverse : logo_aap}></img>
-      </Link>
-      <div className={`${classes.containerMenu} ${!isOpened ? classes.containerMenu__closed : ""}`}>
-        {isArchive ? <MarkHeader /> : <NormalHeader/>}
-      </div>
+    <Fragment>
       {
-        isOpened ?
+        isArchive ? 
+        <Link to={'/'}>
           <img 
-            className={classes.btnMenu}
-            src={btnMenu_rev}
-            onClick={toggleHeader}
-          /> :
+            className={classes.logo}
+            src={logo_aap}
+            style={ isOpened ? { opacity: 1, filter: "invert(100%)" } : { opacity: 0 }}  
+          ></img>
+        </Link> :
+        <Link to={'/'}>
           <img 
-            className={classes.btnMenu}
-            src={btnMenu}
-            onClick={toggleHeader}
-          />
+            className={classes.logo}
+            src={logo_aap}
+            style={ isOpened ? { filter: "invert(100%)" } : { filter: "invert(0%)" }}  
+          ></img>
+        </Link>
       }
-    </header>
+      <header className={`${classes.containerMenu} ${!isOpened ? classes.containerMenu__closed : ""}`}>
+        {isArchive ? <MarkHeader /> : <NormalHeader/>}
+      </header>
+      <img 
+        className={classes.btnMenu}
+        src={btnMenu}
+        style={isOpened ? { filter: "invert(100%)"} : { filter: "invert(0%)"}}
+        onClick={toggleHeader}
+      />
+    </Fragment>
   )
 }
 
